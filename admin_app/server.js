31fd58e7296e698f6f48d3de43415d5aa131399b
Caden MacKenzie <caden.mackenzie@coloradocollege.edu>
2017-04-05 16:01:44 +0000

    require path

--- admin_app/server.js
+++ admin_app/server.js
@@ -1,5 +1,6 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
@@ -9,4 +10,9 @@ app.use(express.static(__dirname + '/www/js'));
 app.set('views', __dirname + '/www/templates');
 // Start the app by listening on the default
 // Heroku port
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);

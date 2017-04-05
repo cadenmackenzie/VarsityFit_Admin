71d21a40bbdd2b0a4a92431240223193496497ed
Caden MacKenzie <caden.mackenzie@coloradocollege.edu>
2017-04-05 16:02:58 +0000

    require path

--- admin_app/server.js
+++ admin_app/server.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/www/templates');
 // Heroku port
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/www/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
